48ec744843e835b4632f5e1c83c56533cee8a713
Mario Riascos <mariofernando07@hotmail.com>
2017-03-08 19:14:44 -0500

    se avanza en la creacion del picturedeatail

---
+++ app/src/main/java/co/com/nexus/platzigram/view/PictureDetailActivity.java
@@ -0,0 +1,15 @@
+package co.com.nexus.platzigram.view;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import co.com.nexus.platzigram.R;
+
+public class PictureDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_picture);
+    }
+}
